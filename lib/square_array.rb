34c7845877ae883605061667b73b4e1b395e11b0
Kai Peng Li <oopanpan@gmail.com>
2020-12-15 23:35:20 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+
+
 def square_array(array)
-  # your code here
+  final_array = []
+  array.length.times do |i|
+    final_array.push(array[i] **2)
+  end
+  final_array
 end
+
+#my_list = [1, 3, 4, 9, 0]
+#square_array(my_list)
